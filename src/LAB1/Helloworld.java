8cc6362eb48ef838422d6d94e1263c3335577308
phiraporn <phirapornkp@nvc.ac.th>
2020-12-15 11:12:16 +0700

    test

---
+++ src/LAB1/Helloworld.java
@@ -0,0 +1,18 @@
+package LAB1;
+
+public class Helloworld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }//main
+
+
+
+
+
+
+}//class
